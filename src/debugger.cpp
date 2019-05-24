9dd07c4a28ab18c0169b454363af2c6cbdb0dadf
Denis Konoplev <denis.konoplev@tra.ai>
2019-05-24 19:16:25 +0300

    Clang format fix

---
+++ src/debugger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by denis_konoplev on 24.05.19.
+//
+
+#include "debugger.h"
---
+++ src/debugger.h
@@ -0,0 +1,16 @@
+//
+// Created by denis_konoplev on 24.05.19.
+//
+
+#ifndef TINYDBG_DEBUGGER_H
+#define TINYDBG_DEBUGGER_H
+
+
+
+class debugger {
+
+};
+
+
+
+#endif //TINYDBG_DEBUGGER_H
